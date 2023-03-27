3eecb3892830b62a9a3ac73f103cfcc6d462043b
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-03-27 12:07:32 +0900

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,49 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.5"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    id("org.springframework.boot") version "2.6.3" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.spring") version "1.6.10" apply false
+    kotlin("plugin.jpa") version "1.6.0" apply false
 }
-
-group = "com.zerobase"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
+allprojects {
+    group = "com.zerobase"
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+    repositories {
+        mavenCentral()
     }
 }
+subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+    }
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+
 }
+
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "fintech"
+include("api")
+include("consumner")
+include("gateway")
+include("css")
+include("domain")
